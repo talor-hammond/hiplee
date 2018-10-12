3bb7fb5d23fe9454e744602a2fa28e465e041a6e
talor-hammond <talorjhammond@gmail.com>
2018-10-12 14:15:21 +1300

    added case for receiving users to reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,12 @@
-
+// Actions:
+import {
+    RECEIVE_USERS
+} from '../actions/users'
 
 export default function users(state = [], action) {
     switch (action.type) {
+        case RECEIVE_USERS:
+            return [...action.users]
         default:
             return state
     }
