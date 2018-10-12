851d0df4acb9c471f10fa2e8c8c31c8c956d9c68
talor-hammond <talorjhammond@gmail.com>
2018-10-12 13:27:24 +1300

    complete button mark-up; centered w flex-box

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello world</h1>
+      <div id="main-container">
+        <div className="button-container">
+          <button className="btn btn-primary">Fetch users</button>
+        </div>
+      </div>
     );
   }
 }
